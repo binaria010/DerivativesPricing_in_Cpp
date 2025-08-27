75acb6bffe5423afea7b3cf96040eaa99b94bbfe
binaria010 <juli.osorio@gmail.com>
2025-08-27 18:16:43 +0100

    fixed smart pointer

--- source/Vanilla.cpp
+++ source/Vanilla.cpp
@@ -1,8 +1,10 @@
 #include <Vanilla.h>
+#include <iostream>
 
 
 // constructor
-VanillaOption::VanillaOption(const PayOff& ThePayOff_, double Expiry_):Expiry(Expiry_){
+VanillaOption::VanillaOption(const PayOff& ThePayOff_, double Expiry_):
+                Expiry(Expiry_){
     // initialize ThePayOff pointer
     ThePayOffPtr = ThePayOff_.clone();
 }
@@ -17,14 +19,15 @@ VanillaOption& VanillaOption::operator =(const VanillaOption& original){
 
     if (this != &original){
         Expiry = original.Expiry;
-        delete ThePayOffPtr;
+        // delete ThePayOffPtr;   // delete for when using raw pointers
         ThePayOffPtr = original.ThePayOffPtr->clone();
     }
     return *this;
 }
 // destructor
 VanillaOption::~VanillaOption(){
-    delete ThePayOffPtr;  // deletes the pointer to PayOff
+    // delete ThePayOffPtr;  // deletes the pointer to PayOff when raw pointer is used
+    std::cout << "VanillaOption destructed. Pointer deleted";
 }
 // Get Expiry method
 double VanillaOption::GetExpiry() const{
